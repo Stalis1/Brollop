33f26f6c46c9db763bddb009a41f4c2d39287385
Stalis1 <henrik.stalbom@hotmail.com>
2025-02-02 15:42:35 +0100

    deployment test commit

--- Web/src/app/page.tsx
+++ Web/src/app/page.tsx
@@ -3,7 +3,7 @@ import { Header } from '../components/header';
 export default function Page() {
     return (
         <div className="flex justify-center">
-            <Header title="Hej" subTitle="wow supercoolt ju" />
+            <Header title="Hej" subTitle="wow supercoolt ju. Test deployment" />
         </div>
     );
 }
