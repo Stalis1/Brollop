0a13a4cd46ad0d4b8231cfe78367c7db1fa5ab78
Stalis1 <henrik.stalbom@hotmail.com>
2025-02-07 13:47:45 +0100

    header menu items

--- Web/src/components/footer.tsx
+++ Web/src/components/footer.tsx
@@ -2,7 +2,7 @@
 export const Footer = () => {
     return (
         <footer className="bg-gray-800 text-white text-center p-4">
-            <h2 className="text-3xl">Footer</h2>
+            <h2 className="text-3xl text-white">Footer</h2>
         </footer>
     );
 };
--- Web/src/components/header.tsx
+++ Web/src/components/header.tsx
@@ -6,8 +6,14 @@ export const Header = ({ title, subTitle }: { title?: string; subTitle?: string 
             <h1 className="text-5xl">{title}</h1>
             <h2 className="text-3xl">{subTitle}</h2>
             <div className="flex flex-row gap-4 items-center justify-center pt-4">
-                <MenuItem link="/" title="Home"/>
-                <MenuItem link="/info" title="Information"/>
+                <MenuItem link="/" title="Välkommen"/>
+                <MenuItem link="/" title="Bröllopet"/>
+                <MenuItem link="/info" title="Info"/>
+                <MenuItem link="/" title="Bilder"/>
+                <MenuItem link="/" title="O.S.A"/>
+                <MenuItem link="/" title="Gästbok"/>
+                <MenuItem link="/" title="Kontakt"/>
+                <MenuItem link="/" title="Önskelista"/>
             </div>
         </div>
     );
