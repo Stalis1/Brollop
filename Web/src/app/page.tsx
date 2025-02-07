4e84745b01c31e63acc89ff4689da7ad3b788535
Stalis1 <henrik.stalbom@hotmail.com>
2025-02-07 13:20:27 +0100

    image and countdown

--- Web/src/app/page.tsx
+++ Web/src/app/page.tsx
@@ -1,7 +1,11 @@
+import Image from "next/image";
+import { Countdown } from "../components/countdown";
+
 export default function Page() {
     return (
-        <div className="flex justify-center">
-            page stuff
+        <div className="flex flex-col items-center">
+            <Image className="m-4" src={"/devImg.png"} alt={"alt"} width={500} height={300}/>
+            <Countdown targetDate="2026-07-01 12:00:00"/>
         </div>
     );
 }
--- Web/src/components/header.tsx
+++ Web/src/components/header.tsx
@@ -1,8 +1,22 @@
+import Link from "next/link";
+
 export const Header = ({ title, subTitle }: { title?: string; subTitle?: string }) => {
     return (
         <div className="flex flex-col justify-center text-center p-4 bg-neutral-400">
             <h1 className="text-5xl">{title}</h1>
             <h2 className="text-3xl">{subTitle}</h2>
+            <div className="flex flex-row gap-4 items-center justify-center pt-4">
+                <MenuItem link="/" title="Home"/>
+                <MenuItem link="/subpage" title="Subpage"/>
+            </div>
         </div>
     );
 };
+
+const MenuItem = ({title, link}: {title: string, link: string}) => {
+    return (
+      <Link href={link}>
+        {title}
+      </Link>
+    );
+  };
