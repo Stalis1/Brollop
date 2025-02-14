11b2fb148607bb07c27b3b5f410d77eb8bb17199
Stalis1 <henrik.stalbom@hotmail.com>
2025-02-14 11:21:44 +0100

    images

--- Web/src/app/page.tsx
+++ Web/src/app/page.tsx
@@ -4,7 +4,7 @@ import { Countdown } from "../components/countdown";
 export default function Page() {
     return (
         <div className="flex flex-col items-center">
-            <Image className="m-4" src={"/devImg.png"} alt={"alt"} width={500} height={300}/>
+            <Image className="m-4" src={"/Pose.jpg"} alt={"alt"} width={500} height={300}/>
             <div className="text-3xl">01 juli 2026</div>
             <Countdown targetDate="2026-07-01 12:00:00"/>
         </div>
