3ba40a746a103c08f96e98c813cd6a3702d14fed
Stalis1 <henrik.stalbom@hotmail.com>
2025-10-04 18:41:57 +0200

    OSA - allergies

--- Web/src/components/board/osa-board .tsx
+++ Web/src/components/board/osa-board .tsx
@@ -4,8 +4,7 @@ import { Board, Line } from './board';
 export const OsaBoard = () => {
     return (
         <Board>
-            <h1 className="text-4xl">Osa</h1>
-            OBS: lägg till allergier
+            <h1 className="text-4xl">OSA</h1>
             <Line />
             <div className="my-4 p-4 bg-peach">
                 <GoogleFormSubmit />
