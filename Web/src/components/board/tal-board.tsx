0bdbe715f9c77088d3fd570953a6892469d7667e
Stalis1 <henrik.stalbom@hotmail.com>
2025-12-27 15:59:41 +0100

    speech board

--- Web/src/components/board/tal-board.tsx
+++ Web/src/components/board/tal-board.tsx
@@ -7,9 +7,10 @@ export const TalBoard = () => {
             <h1 className="text-4xl">Tal</h1>
             <p>Vad roligt att du hittat till detta formulär. 
                 Här anmäler ni intresse för att hålla tal på Gabriella och Henriks bröllop. 
-                Ungefär en månad innan bröllopet kommer ett uppföljningsformulär, där ni kan fylla i mer information om talet. 
+                Ungefär en månad innan bröllopet kommer toastmasters skicka ut ett uppföljningsformulär, där ni kan fylla i mer information om talet. 
                 Anmälan är inte bindande och vi hör av oss i början av juni. 
                 Skicka in din anmälan om tal senast den 14:e juni.</p>
+                <p>Anmälan kommer ej synas för brudparet.</p>
             <Line />
             <div className="my-4 p-4 bg-peach">
                 <FormSubmitSpeech />
