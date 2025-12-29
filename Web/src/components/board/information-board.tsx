1ebc98b6f07f925e0c2f82b5b26dc088f846c6d3
Stalis1 <henrik.stalbom@hotmail.com>
2025-12-29 18:28:55 +0100

    spelling

--- Web/src/components/board/information-board.tsx
+++ Web/src/components/board/information-board.tsx
@@ -15,12 +15,12 @@ export const InformationBoard = () => {
                     <BoardLink link={'https://www.kladkoder.nu/kavaj/#:~:text=kl%C3%A4%20dig%20i!-,Kavaj,-Kavaj%20%C3%A4r%20en'} title={'vad innebär det?'} />
                 </p>
                 Vi tycker inte klädkoden behöver vara så strikt, men vi ser gärna att ni kommer uppklädda i snygga plagg! Undvik i vanlig ordning vitt, rött och
-                gärna även svart. Det får gärna vara färgglatt, så det är ett utmärkt tillfälle att ta fram den där orangea kostymen!
+                gärna även svart. Det får gärna vara färgglatt, så det är ett utmärkt tillfälle att ta fram den där orange kostymen!
             </Section>
             <Line />
             <Section title="Boende">För boende rekommenderas Lund eller Kävlinge.</Section>
             <Line />
-            <Section title="Transport">Vigsel och fest är på samma plats. Transport till och från annordnas själv.</Section>
+            <Section title="Transport">Vigsel och fest är på samma plats. Transport till och från anordnas själv.</Section>
             <Line />
             <Section title="Tal">Om du vill hålla tal behöver du anmäla via formulär här på hemsidan i förväg.</Section>
             <Line />
@@ -31,10 +31,8 @@ export const InformationBoard = () => {
             <Section title="Schema">
                 <BulletPoints
                     points={[
-                        'TBD',
                         '15:30: Vigsel',
                         '02:00: Stad i ljus.'
-                        //'13:00: Vigsel', '15:00: Mingel', '17:00: Middag', 'etc etc'
                     ]}
                 />
             </Section>
